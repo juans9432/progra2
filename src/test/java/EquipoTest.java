c2e0dea5586d6698b66f3e33979c69eef7d122f2
vanessahg07 <vanehenaog05@gmail.com>
2025-03-06 14:34:41 -1100

    método para calcular el porcentaje del salario de los jugadores

--- src/test/java/EquipoTest.java
+++ src/test/java/EquipoTest.java
@@ -8,7 +8,7 @@ public class EquipoTest {
     private Equipo equipo;
 
     @Test
-    public void despedirJugadorTest(){
+    public void despedirJugadorTest() throws Exception {
         Equipo equipo = new Equipo("Nacional");
         JugadorEnum jugador1 = new JugadorEnum("Pedro", 500000, 3.7f, 18, Posicion.DEF, LocalDate.of(2020, 9, 14));
         JugadorEnum jugador2 = new JugadorEnum("Samuel", 450000, 4.6f, 10, Posicion.DEL, LocalDate.of(2019, 4, 30));
